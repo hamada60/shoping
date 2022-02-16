2e2033cc0ddb74e4f28a8b4978e70560cf02b65c
hamada60 <hamada.magdy9900@gmail.com>
2022-02-16 11:48:16 +0200

    footer cmoponent

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
     <div className="layout">
       <Header />
       <main>m</main>
-      <footer>f</footer>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../css/Footer/Footer.css';
+
+export default function Footer() {
+  return (
+    <div>
+        <footer>f</footer>
+    </div>
+  )
+}
