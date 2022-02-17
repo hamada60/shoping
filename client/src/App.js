90b70d8a83f37db5693f0002831b13bd7148a26f
hamada60 <hamada.magdy9900@gmail.com>
2022-02-17 15:28:22 +0200

    product details

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
+import data from './data.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 function App() {
+  const [products, setProducts] = useState(data);
+  console.log(products);
   return (
     <div className="layout">
       <Header />
-      <main>m</main>
+      <main>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8">
+              <Products products={products} />
+            </div>
+            <div class="col-md-4">
+              <div className='filter-wrapper'>fillter</div>
+            </div>
+          </div>
+        </div>
+      </main>
       <Footer />
     </div>
   );
